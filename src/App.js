b5d5ea7c7f1d70a576e4f7d8eb5f243452e595ff
Mohd Zain <zainmohd1998@gmail.com>
2024-06-11 13:09:49 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import{BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import Signup from './Components/Signup';
+import Login from './Components/Login'
+import Alert from './Components/Alert';
+import { useState,useEffect } from 'react';
+import { useSelector } from 'react-redux'
+import UserState from './Context/UserState';
+import Chatbox from './Components/Chatbox'
+
 
 function App() {
+  const [alert, setAlert] = useState(null)
+  const [login, setLogin] = useState(true)
+  const type = useSelector(state => state.type)
+  const host = 'http://localhost:5001'
+
+  useEffect(()=>{
+    if(localStorage.getItem('token')){
+      setLogin(false)
+    }
+     // eslint-disable-next-line
+  },[])
+
+  const showAlert = (message, type) =>{
+    setAlert({
+      msg : message,
+      type :type
+    })
+    setTimeout(() =>{
+      setAlert(null)
+    },2000)
+  }
+  const Logdin = () => {
+    setLogin(true);
+  };
+
+  const Logdout = () => {
+    setLogin(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+    <UserState host={{host}}>
+    <Router>
+    <Navbar prop={{host,Logdin,showAlert}}/>
+    <Alert alert={alert}/>
+    <Routes>
+        <Route exact path="/" element={<Home prop={{host, showAlert, Logdin, Logdout, login}}/>}></Route>
+        <Route exact path='/login' element={<Login prop={{host, showAlert, Logdout}}/>}></Route>
+        <Route exact path='/signup' element={<Signup prop={{host, showAlert, Logdout}}/>}></Route>
+        <Route exact path='/chat' element={<Chatbox prop={{host, showAlert}}/>}></Route>
+        <Route exact path={`/chat/${type}`} element={<Chatbox prop={{host, showAlert}}/>}></Route>
+        {/* <Route exact path='/chat/txt' element={<Chatbox prop={{host, showAlert}}/>}></Route>
+        <Route exact path='/chat/csv' element={<Chatbox prop={{host, showAlert}}/>}></Route>
+        <Route exact path='/chat/xlsx' element={<Chatbox prop={{host, showAlert}}/>}></Route>
+        <Route exact path='/chat/sql' element={<Chatbox prop={{host, showAlert}}/>}></Route> */}
+    </Routes>
+    </Router>
+    </UserState>
     </div>
+    </>
   );
 }
 
