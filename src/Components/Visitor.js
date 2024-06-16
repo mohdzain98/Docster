02c4c447953c6e80abbcd2e0bd2f2f4d6c7bd330
Mohd Zain <zainmohd1998@gmail.com>
2024-06-16 14:21:40 +0530

    add visitor count

---
+++ src/Components/Visitor.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Visitor = () => {
+  return (
+    <div className='container mx-3' id='visit'>
+       <div className='box'>
+       <script type="text/javascript" src="https://www.freevisitorcounters.com/en/home/counter/1193231/t/1"></script>
+       <div id="sfc2hwmmda31umddkr5wjxrfs42ymgwn8km"></div><script type="text/javascript" src="https://counter4.optistats.ovh/private/counter.js?c=2hwmmda31umddkr5wjxrfs42ymgwn8km&down=async" async></script><noscript><a href="https://www.freecounterstat.com" title="free traffic counter"><img src="https://counter4.optistats.ovh/private/freecounterstat.php?c=2hwmmda31umddkr5wjxrfs42ymgwn8km" border="0" title="free traffic counter" alt="free traffic counter"/></a></noscript>
+    
+       <a href="https://www.freecounterstat.com" title="free traffic counter"><img src="https://counter4.optistats.ovh/private/freecounterstat.php?c=2hwmmda31umddkr5wjxrfs42ymgwn8km" border="0" title="free traffic counter" alt="free traffic counter"/></a>
+       </div>
+    </div>
+  )
+}
+
+export default Visitor
