9411e423adf63896f2801fda5fa58e5eab463e02
Mohd Zain <zainmohd1998@gmail.com>
2024-06-21 22:17:15 +0530

    add version

--- Microservices/User_auth/index.js
+++ Microservices/User_auth/index.js
@@ -16,7 +16,8 @@ app.use('/api/moretokens',require('./routes/moretokens') )
 
 app.use('/',(req,res)=>{
   return res.json({
-    message:"Wecome to Docster user auth microservice"
+    message:"Wecome to Docster user auth microservice",
+    version:"1.1"
   })
 })
 
