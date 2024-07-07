5a64e8904dc96fd038510d250813ebeb8e97eac8
Mohd Zain <zainmohd1998@gmail.com>
2024-07-07 14:52:49 +0530

    remove navbar icon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -121,9 +121,6 @@ const Navbar = (props) => {
             </div>
         <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-                <img src="apple-touch-icon.ico" alt="" width="30" height="30"/>
-                </Link>
                 <Link className="navbar-brand" to="/">Docschat</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" ref={ref}>
                 <span className="navbar-toggler-icon"></span>
