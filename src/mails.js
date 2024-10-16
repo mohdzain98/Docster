1598a2f8e39765148147caebb3fd541409f5029c
Mohd Zain <zainmohd1998@gmail.com>
2024-10-16 20:26:18 +0530

    update invitation mail

--- src/mails.js
+++ src/mails.js
@@ -6,14 +6,23 @@ const templates ={
                 <center><img src="https://drive.google.com/thumbnail?id=1g8bbF8UZo_ylH4vSbGTKS2oOemw-7rYO&sz=w1000" width='256px'></center>
 
             <h4 style="color: #6c757d; margin: 1rem 0;">Hi there, Welcome to Docschat</h4>
-            <p >We are happy to have you, enjoy limitless conversation with your documents, Chat with <span style="color: brown;">PDF, TXT, CSV, Excel and SQL file.<span></p>
-            <p style="margin-top: -1%;">Our service is built with industry-standard security measures to protect your data. We prioritize the privacy and security of your information..</p>
+            <p style="text-align: justify;">We are happy to have you, enjoy limitless conversation with your documents, Chat with <span style="color: brown;">PDF, TXT, CSV, Excel, PPT, DOCX and SQL file.<span></p>
+            <p style="text-align: justify;">Moreover, try advanced tools for content retrieval 
+                <ul style="text-align:justify; margin-top: -1%;">
+                    <li>Exa Search: advanced browser search tool</li>
+                    <li>Google Trends: find trending searches on google for a topic</li>
+                    <li>Google Scholar: finds scholarly research articles</li>
+                    <li>PubMed: Provides citation for biomedical literature</li>
+                </ul>
+            </p>
+            <p style="text-align:justify">Our service is built with industry-standard security measures to protect your data. We prioritize the privacy and security of your information..</p>
             <p>Don't forget to give <a href="https://docschat.in/contactus">Feedback</a> or if you face any issue kindly report <a href="https://docschat.in/contactus">here</a></p>
+            <p>Visit Now: <a href="https://docschat.in">https://docschat.in</a></p>
         </div>
     </div>
     <div id="footer" class="box" style="background-color: lightgrey; width: auto; position:relative; box-shadow: 0 25px 25px rgba(0, 0, 0, 0.1); padding: 25px;">
             <p>Best Regards</p>
-            <p style="margin-top: -1%;">Docschat</p>
+            <p style="margin-top: -15px;">Docschat</p>
             <ul style="list-style: none; padding: 0;">
                 <li style="display: inline; margin-right: 5px;"><a href="https://docschat.in" style="text-decoration: none;">View Site</a></li>
                 <li style="display: inline; margin-right: 5px"><a href="https://docschat.in/privacy-policy" style="text-decoration: none;">Privacy Policy</a></li>
